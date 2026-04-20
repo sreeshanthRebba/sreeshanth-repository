6c125327ffb61614220b2fe62a0e829e6d9d253c
sreeshanth <rebbasreeshanth@gmail.com>
2026-04-20 12:14:13 +0530

    varchange

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -9,14 +9,14 @@ int factorial(int n) {
 }
 
 int main(){
-    int number;
+    int num;
     cout << "Enter a number to calculate its factorial: ";
-    cin >> number;
+    cin >> num;
 
-    if (number < 0) {
+    if (num < 0) {
         cout << "Factorial is not defined for negative numbers." << endl;
     } else {
-        cout << "Factorial of " << number << " is " << factorial(number) << endl;
+        cout << "Factorial of " << num << " is " << factorial(num) << endl;
     }
 
     return 0;
